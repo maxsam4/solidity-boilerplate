41e48a3bdf5039ec2e0c72a14f618375928ad636
Mudit Gupta <guptamudit@ymail.com>
2019-06-09 12:49:53 +0530

    Updated truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,34 @@
+require('dotenv').config();
+const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 7545,
+      port: 8545,
       network_id: '5777'
+    },
+    kovan: {
+      provider: () => {
+        let wallet = new HDWalletProvider(process.env.PRIVATE_KEY, process.env.KOVAN_ENDPOINT)
+        var nonceTracker = new NonceTrackerSubprovider()
+        wallet.engine._providers.unshift(nonceTracker)
+        nonceTracker.setEngine(wallet.engine)
+        return wallet
+      },
+      network_id: '42', // Match any network id
+      gasPrice: 5000000000 // 5 gwei
     }
   },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   },
   mocha: {
